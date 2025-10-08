9dec68a16e6cd9d0cd4c9cea59a5e14dbb42d80f
Alex-Ubuntu-Windows <alejandro.ramos.gua@gmail.com>
2025-10-08 16:00:01 +0100

    almost done with 00 need to sort the spaces and makefile

---
+++ CPP00/ex00/megaphone.cpp
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aramos <alejandro.ramos.gua@gmail.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/08 15:12:37 by aramos            #+#    #+#             */
+/*   Updated: 2025/10/08 15:12:39 by aramos           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	IsLower(char c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	return (0);
+}
+
+void	ToUpper(char *str)
+{
+	int	i;
+
+	i = 0;
+	while(str[i])
+	{
+		if (IsLower(str[i]))
+			std::cout.put(str[i] - 32);
+		else
+			std::cout.put(str[i]);
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	i = 1;
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+	{
+		while (argv[i])
+		{
+			ToUpper(argv[i]);
+			if (argv[i + 1])
+				std::cout.put(' ');
+			i++;
+		}
+	}
+	std::cout << "\n";
+	return (0);
+}
