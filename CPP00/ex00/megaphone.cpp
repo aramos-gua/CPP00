7d3ccf89d53e8596e382c27dede7abc52b2b11ec
Alex <alejandro.ramos.gua@gmail.com>
2025-10-14 20:59:31 +0100

    needs fixing frickingspaes

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -12,31 +12,47 @@
 
 #include <iostream>
 
-int	IsLower(char c)
+// int	IsLower(char c)
+// {
+// 	if (c >= 'a' && c <= 'z')
+// 		return (1);
+// 	return (0);
+// }
+//
+// void	ToUpper(char *str)
+// {
+// 	int	i;
+//
+// 	i = 0;
+// 	while(str[i])
+// 	{
+// 		if (IsLower(str[i]))
+// 			std::cout.put(str[i] - 32);
+// 		else
+// 			std::cout.put(str[i]);
+// 		i++;
+// 	}
+// }
+
+// int	IsLower(char c)
+// {
+// 	if (c >= 'a' && c <= 'z')
+// 		return (1);
+// 	return (0);
+// }
+
+void	ToUpper(char c)
 {
 	if (c >= 'a' && c <= 'z')
-		return (1);
-	return (0);
-}
-
-void	ToUpper(char *str)
-{
-	int	i;
-
-	i = 0;
-	while(str[i])
-	{
-		if (IsLower(str[i]))
-			std::cout.put(str[i] - 32);
-		else
-			std::cout.put(str[i]);
-		i++;
-	}
+		std::cout.put(c - 32);
+	else
+		std::cout.put(c);
 }
 
 int	main(int argc, char **argv)
 {
 	int	i;
+	int	j;
 
 	i = 1;
 	if (argc == 1)
@@ -45,9 +61,14 @@ int	main(int argc, char **argv)
 	{
 		while (argv[i])
 		{
-			ToUpper(argv[i]);
-			if (argv[i + 1])
-				std::cout.put(' ');
+			j = 0;
+			while (argv[i][j])
+			{
+				if ((argv[i + 1] && argv[i + 1][0] != ' ') || (argv[i][j + 1] == '\0' && argv[i][j] != ' '))
+					std::cout.put(' ');
+				ToUpper(argv[i][j]);
+				j++;
+			}
 			i++;
 		}
 	}
