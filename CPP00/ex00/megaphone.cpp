f5dcd0d2b8975c86ca82646a57b424c064ef6056
alex-gua <alejandro.ramos.gua@gmail.com>
2025-10-15 13:57:52 +0100

    apparently ex00 is done

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -12,65 +12,23 @@
 
 #include <iostream>
 
-// int	IsLower(char c)
-// {
-// 	if (c >= 'a' && c <= 'z')
-// 		return (1);
-// 	return (0);
-// }
-//
-// void	ToUpper(char *str)
-// {
-// 	int	i;
-//
-// 	i = 0;
-// 	while(str[i])
-// 	{
-// 		if (IsLower(str[i]))
-// 			std::cout.put(str[i] - 32);
-// 		else
-// 			std::cout.put(str[i]);
-// 		i++;
-// 	}
-// }
-
-// int	IsLower(char c)
-// {
-// 	if (c >= 'a' && c <= 'z')
-// 		return (1);
-// 	return (0);
-// }
-
-void	ToUpper(char c)
-{
-	if (c >= 'a' && c <= 'z')
-		std::cout.put(c - 32);
-	else
-		std::cout.put(c);
-}
-
 int	main(int argc, char **argv)
 {
 	int	i;
 	int	j;
 
 	i = 1;
-	if (argc == 1)
+	if (argc < 2)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	else
 	{
 		while (argv[i])
 		{
-			j = 0;
-			while (argv[i][j])
-			{
-				if ((argv[i + 1] && argv[i + 1][0] != ' ') || (argv[i][j + 1] == '\0' && argv[i][j] != ' '))
-					std::cout.put(' ');
-				ToUpper(argv[i][j]);
-				j++;
-			}
-			i++;
-		}
+      j = 0;
+      while (i > 0 && argv[i][j])
+        std::cout << (char)std::toupper(argv[i][j++]);
+      i++;
+    }
 	}
 	std::cout << "\n";
 	return (0);
