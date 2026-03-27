80297861fa5ab8b950d3dbda1e3203da9668a3eb
alex-gua <alejandro.ramos.gua@gmail.com>
2026-03-27 10:03:57 +0000

    finished ex04

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -26,26 +26,25 @@ int	main(int argc, char **argv)
 	std::string		s2;
 	std::string		line;
 	std::string		fileName = argv[1];
-	std::ifstream	inFile(fileName);
-	std::ofstream	outFile(fileName + ".replace");
+	std::ifstream	inFile(fileName.c_str());
+	std::ofstream	outFile((fileName + ".replace").c_str());
 
 	s1 = argv[2];
 	s2 = argv[3];
+	if (s1.empty())
+		return (1);
 	if (inFile && outFile)
 	{
 		while(std::getline(inFile, line))
 		{
 			pos = 0;
+			while ((pos = line.find(s1, pos)) != std::string::npos)
 			{
-				if (s1.empty() || s2.empty())
-					return (1);
-				while ((pos = line.find(s1, pos)) != std::string::npos)
-				{
-					line.erase(pos, s1.length());
-					line.insert(pos, s2);
-					pos += s2.size();
-				}
+				line.erase(pos, s1.length());
+				line.insert(pos, s2);
+				pos += s2.size();
 			}
+			outFile << line << std::endl;
 		}
 	}
 	return (0);
