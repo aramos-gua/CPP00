574529d12cf8db4cc5311fe6d8cfc39bcf9e0f17
alex-gua <alejandro.ramos.gua@gmail.com>
2026-04-01 10:32:27 +0100

    started ex00

---
+++ CPP02/ex00/Fixed.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aramos <contact@aramos.dev>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/31 11:29:01 by aramos            #+#    #+#             */
+/*   Updated: 2026/04/01 10:32:12 by aramos           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+
+class	Fixed
+{
+	private:
+		int					value;
+		static const int	fractional_bits = 8;
+	public:
+		Fixed();
+		Fixed(const Fixed &other);
+		Fixed &operator=(const Fixed &other);
+		~Fixed();
+
+		int		getRawBits(void) const;
+		void	setRawBits(int const raw);
+};
+
+# endif
