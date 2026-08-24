06a8a97504079f50623a31242e7eb8c5f37bb8dd
aramos-gua <alejandro.ramos.gua@gmail.com>
2026-08-24 12:55:11 +0100

    started working on cpp06

---
+++ CPP06/ex00/inc/ScalarConverter.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aramos <alejandro.ramos.gua@gmail.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/24 12:45:22 by aramos            #+#    #+#             */
+/*   Updated: 2026/08/24 12:52:00 by aramos           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALARCONVERTER_HPP
+# define SCALARCONVERTER_HPP
+
+class ScalarConverter
+{
+	private :
+		ScalarConverter();
+		ScalarConverter(const ScalarConverter &other);
+		ScalarConverter &operator=(const ScalarConverter &other);
+		~ScalarConverter();
+
+	public :
+		static void	convert(const std::string &literal);
+};
+
+#endif
