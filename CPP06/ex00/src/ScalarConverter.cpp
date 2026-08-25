0c9a4db8b56957716d704eac8894d321eee0117f
aramos-gua <alejandro.ramos.gua@gmail.com>
2026-08-25 13:54:26 +0200

    need to write convert method

--- CPP06/ex00/src/ScalarConverter.cpp
+++ CPP06/ex00/src/ScalarConverter.cpp
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ScalarConverter.hpp"
+#include "../inc/ScalarConverter.hpp"
 
 enum	LiteralType
 {
@@ -21,11 +21,56 @@ enum	LiteralType
 	INVALID
 };
 
+size_t	detectPseudoLiteral(const std::string &literal)
+{
+	if (literal == "-inf" || literal == "+inf" || literal == "nan" )
+		return (1);
+	else if (literal == "-inff" || literal == "+inff" || literal == "nanf")
+		return (2);
+	else
+		return (3);
+}
+
+bool	allNumbers(const std::string &literal)
+{
+	size_t	i = 0;
+
+	if (literal[i] && (literal[i] == '-' || literal[i] == '+'))
+			i++;
+	while (i < literal.size())
+	{
+		if (literal[i] < 48 || literal[i] > 57)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
 LiteralType	detectType(const std::string &literal)
 {
+	size_t	pos = literal.find(".");
+
+	if (literal.empty())
+		return (INVALID);
 	if (literal.size() == 3 && literal[0] == '\'' && literal[2] == '\'')
-	{
-		if (literal[1] > 29 && literal[1] < 127)
-			return (CHAR);
-	}
+		return (CHAR);
+	else if (detectPseudoLiteral(literal) == 1)
+		return (DOUBLE);
+	else if (detectPseudoLiteral(literal) == 2)
+		return (FLOAT);
+	else if (literal[literal.size() - 1] == 'f')
+		return (FLOAT);
+	else if (pos != std::string::npos)
+		return (DOUBLE);
+	else if (allNumbers(literal))
+		return (INT);
+	return (INVALID);
+}
+
+
+void	ScalarConverter::convert(const std::string &literal)
+{
+	LiteralType	type = detectType(literal);
+	double		value;
+	switch (type)
 }
